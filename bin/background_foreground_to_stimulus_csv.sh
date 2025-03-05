566e84a5ba148e3bb7c2d5bb6a70424e3134772d
suzannejin <suzanne.jin.yao@gmail.com>
2025-03-05 12:47:08 +0100

    add bin/background_foreground_to_stimulus_csv.sh

---
+++ bin/background_foreground_to_stimulus_csv.sh
@@ -0,0 +1,22 @@
+BEGIN {
+    print "sequence,label"
+}
+
+# Process background file (first file)
+FNR==NR {
+    if (!/^>/) {
+        gsub(/[[:space:]]/,"")
+        if (length($0) > 0) {
+            print $0 ",0"
+        }
+    }
+    next
+}
+
+# Process foreground file (second file)
+!/^>/ {
+    gsub(/[[:space:]]/,"")
+    if (length($0) > 0) {
+        print $0 ",1"
+    }
+}
